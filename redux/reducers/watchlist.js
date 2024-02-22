851fef7540157dc4051c83edb63dab8fb28030a0
Shubham Kalamkar <shubham.mobicloud@gmail.com>
2024-02-22 18:01:38 +0530

    toast add

--- redux/reducers/watchlist.js
+++ redux/reducers/watchlist.js
@@ -1,3 +1,4 @@
+import { ToastAndroid } from 'react-native';
 import {REMOVE_WATCHLIST_STOCK, SET_WATCHLIST_STOCKS} from '../constants';
 
 const initialState = [];
@@ -6,11 +7,13 @@ export const watchlist = (state = initialState, action) => {
   switch (action.type) {
     case SET_WATCHLIST_STOCKS:
       console.warn('Stock Added Successfully ');
+        ToastAndroid.show('Stock Added Successfully', ToastAndroid.SHORT);
       return [...state, ...action.watchlistStocks];
 
     case REMOVE_WATCHLIST_STOCK:
       state = state.filter(s => s.symbol !== action.stock.symbol);
       console.warn("Stock Removed Successfully")
+      ToastAndroid.show('Stock Removed Successfully', ToastAndroid.SHORT);
       return state;
     default:
       return state;
